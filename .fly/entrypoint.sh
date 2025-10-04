e4993cf84f40c9cd6dda1bab569c5596ad71c557
ricardoceli-spec <ricardo.celi@utelvt.edu.ec>
2025-10-04 00:51:21 -0500

    Update entrypoint.sh

--- .fly/entrypoint.sh
+++ .fly/entrypoint.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
 
-# Run user scripts, if they exist
+# Ejecutar scripts de usuario si existen
 for f in /var/www/html/.fly/scripts/*.sh; do
-    # Bail out this loop if any script exits with non-zero status code
+    # Salir si algún script falla
     bash "$f" -e
 done
+
+# Ajustar permisos
 chown -R www-data:www-data /var/www/html
 
+# Ejecutar Laravel Artisan al iniciar el contenedor
+php /var/www/html/artisan package:discover --ansi
+# Opcional: migraciones automáticas
+# php /var/www/html/artisan migrate --force
+
+# Ejecutar supervisord o comando pasado al contenedor
 if [ $# -gt 0 ]; then
-    # If we passed a command, run it as root
     exec "$@"
 else
     exec supervisord -c /etc/supervisor/supervisord.conf
